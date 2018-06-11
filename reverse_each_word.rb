281290b4bb46038a03c02646ca88cbc98c094c39
Sean Conrad <seanconrad123@gmail.com>
2018-06-11 17:44:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,10 +3,10 @@ cool_nums = [1, 2, 3]
 def reverse_each_word(string)
   string = string.split
   print string
-  string.each do |x|
+  '''string.each do |x|
     x.reverse
     return x
-  end
+  end'''
 
 end
 
