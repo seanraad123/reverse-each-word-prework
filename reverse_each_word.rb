91e2c818b203d8fb89bab5738b1b45dd8af9e29c
Sean Conrad <seanconrad123@gmail.com>
2018-06-11 17:29:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
-def reverse_each_word(string)
-  string.each do |student|
-    puts student
-  end
+def turn_into_frog(student)
+  puts "Poof! #{student} is a frog."
 end
-
-reverse_each_word("test!! this is a test!")
